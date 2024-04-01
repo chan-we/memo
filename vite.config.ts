b3a943669ef51be8878a7341fc8ca5d6bf182918
chan_we <chan_we@foxmail.com>
2024-04-01 23:28:13 +0800

    feat: no path

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/memo/',
+  base: '/',
   plugins: [
     vue(),
     AutoImport({
